7a71f1bef08466928cbb641b5d50d52ffb8451cf
Vanessa Roy <vr.vanessa.roy@gmail.com>
2024-01-08 11:41:57 +0100

    create the endpoint patients to get all the patients

--- patientService/src/main/java/com/medilabosolutions/patientService/controller/PatientController.java
+++ patientService/src/main/java/com/medilabosolutions/patientService/controller/PatientController.java
@@ -1,12 +1,23 @@
 package com.medilabosolutions.patientService.controller;
 
+import com.medilabosolutions.patientService.model.Patient;
 import com.medilabosolutions.patientService.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class PatientController {
 
     @Autowired
     private PatientService patientService;
+
+    @GetMapping("/patients")
+    public List<Patient> getPatients() {
+        return patientService.findAll();
+    }
+
+
 }
