b23ae2ba9132e96e334a4f2ad68445ee2cb02b3b
Vanessa Roy <vr.vanessa.roy@gmail.com>
2024-01-16 10:37:23 +0100

    update PatienDTO and patients template

--- medilaboSolutions/clientService/src/main/java/com/medilabosolutions/clientService/controller/dtos/PatientDTO.java
+++ medilaboSolutions/clientService/src/main/java/com/medilabosolutions/clientService/controller/dtos/PatientDTO.java
@@ -15,4 +15,8 @@ public class PatientDTO {
     private Gender gender;
     private String address;
     private String phone;
+
+    public String getPhoneFormat() {
+        return phone.substring(0,3) + "-" + phone.substring(3,6) + "-" + phone.substring(6);
+    }
 }
